c7c8566d08281023c057cb5917aeefd520c56364
Ann Gutuleac <ann.gutuleac@gmail.com>
2023-06-21 14:49:13 +0300

    Make lesson 5 homework

--- src/QDemo.java
+++ src/QDemo.java
@@ -36,7 +36,7 @@ class QDemo {
         System.out.println("Иcпoльзoвaниe очереди bigQ для сохранения алфавита");
         // Поместить буквенные символы в очередь ЬigQ
         for(i = 0; i < 26; i++)
-            bigQ.put((char) ('А' + i));
+            bigQ.put((char) ('A' + i));
         // Извлечь буквенные символы из очереди ЬigQ и отобразить
         System.out.print("Coдepжимoe очереди bigQ: ");
         for (i = 0; i < 26; i++) {
@@ -46,18 +46,18 @@ class QDemo {
 
         System.out.println("\n");
         System.out.println("Иcпoльзoвaниe очереди smallQ для генерации ошибок");
-// Использовать очередь srnallQ для генерации ошибок
+        // Использовать очередь smallQ для генерации ошибок
         for(i = 0; i < 5; i++) {
-            System.out.print("Пoпыткa сохранения " + (char)('z' - i));
-            smallQ.put((char)('Z' - i));
+            System.out.print("Пoпыткa сохранения " + (char) ('Z' - i));
+            smallQ.put((char) ('Z' - i));
             System.out.println();
-            System.out.println();
-// Дополнительные ошибки при обращении к очереди smallQ
-            System.out.print("Coдepжимoe smallQ: ");
-            for(i = 0; i < 5; i++) {
-                ch = smallQ.get();
-                if (ch != (char)0) System.out.print(ch);
-            }
+        }
+        System.out.println();
+        // Дополнительные ошибки при обращении к очереди smallQ
+        System.out.print("Coдepжимoe smallQ: ");
+        for(i = 0; i < 5; i++) {
+            ch = smallQ.get();
+            if (ch != (char)0) System.out.print(ch);
         }
     }
 }
